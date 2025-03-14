f8918b1c2f693e0ba4e2703d50481ba982d73976
wind5z1 <133775708+wind5z1@users.noreply.github.com>
2025-03-14 15:20:23 +0900

    Updated

--- server/Chatbot.py
+++ server/Chatbot.py
@@ -234,7 +234,20 @@ def get_joke():
             return "I didn't have any jokes at this moment.Let me think a bit..."
     except Exception as e:
         return "An error occurred while fetching a joke."
-    
+def get_news():
+    api_key="1087b5b718d54d5293369d275c13d2d8"
+    url=f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            articles=response.json().get("articles", [])
+            news_list=[f"{article['title']}:{article['description']}"for article in articles]
+            return "Here are the latest news headlines in us:\n" + "\n".join(news_list)
+        else:
+            return "I didn't have any news at this moment.Let me think a bit..."
+    except Exception as e:
+        return f"An error occurred while fetching news."
 def get_weather(city, day_offset):
     api_key = "5dac3b051c407fc1fcc3b4d8e6043446"
     if day_offset == 0:
@@ -278,6 +291,9 @@ def generate_response(user_input):
         if time_response:
             return time_response
 
+        if "news" in user_input.lower() or "headlines" in user_input.lower():
+            return get_news()
+        
         if re.search(r"how about(.+)", user_input.lower()):
             new_query = re.search(r"how about(.+)", user_input.lower()).group(1).strip()
             if context_memory["last_translation"]:
