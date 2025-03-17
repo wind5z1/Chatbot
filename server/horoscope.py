f5b642f5addebf9c06b2c90856a4d5474456bce8
wind5z1 <133775708+wind5z1@users.noreply.github.com>
2025-03-17 17:15:30 +0900

    Updated

--- server/horoscope.py
+++ server/horoscope.py
@@ -1,10 +1,10 @@
 import requests
 
 def get_horoscope(zodiac_sign):
-    url=f"https://horoscope-api.herokuapp.com/horoscope/today/{zodiac_sign}"
+    url=f"https://aztro.sameerkumar.website?sign={zodiac_sign}&day={day}"
     response = requests.get(url)
     if response.status_code == 200:
         horoscope_data = response.json()
-        return horoscope_data["horoscope"]
+        return horoscope_data["description"]
     else:
         return "I didn't have any horoscope imformation at this moment.Let me think a bit..."
