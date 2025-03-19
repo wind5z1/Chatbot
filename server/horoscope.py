39e38358c6c8c8daa76599009ecec2574a5ac6e4
wind5z1 <133775708+wind5z1@users.noreply.github.com>
2025-03-19 16:44:45 +0900

    Updated

--- server/horoscope.py
+++ server/horoscope.py
@@ -1,14 +1,18 @@
 import requests
 
 def get_horoscope(sign):
-    url = f"https://best-daily-astrology-and-horoscope-api.p.rapidapi.com/api/Detailed-Horoscope/?zodiacSign={sign}"
+    url = "https://best-daily-astrology-and-horoscope-api.p.rapidapi.com/api/Detailed-Horoscope/"
+    querystring = {"zodiacSign": sign}
+
     headers = {
         "X-RapidAPI-Key": "0bde6d52ccmsh7e12e7c52ee3d68p119f1djsn84b655af5bb6",  # 替換為你的 RapidAPI 密鑰
         "X-RapidAPI-Host": "best-daily-astrology-and-horoscope-api.p.rapidapi.com"
     }
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return "Failed to fetch horoscope."
+    try:
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return "I didn't have any horoscope at this moment.Let me think a bit..."
+    except requests.exceptions.RequestException as e:
+        return f"An Error occured: {e}"
